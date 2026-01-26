d940e8cd8afc429fd30dc5345914398f631a9ab6
rendor535 <marcinbos2004@gmail.com>
2026-01-26 18:12:16 +0100

    to co wcześniej

--- server/DTOs/Session/CreateSessionRequest.cs
+++ server/DTOs/Session/CreateSessionRequest.cs
@@ -3,6 +3,5 @@ public class CreateSessionRequest
 {
     /// <example>Impreza u Stefana</example>
     public string Name { get; set; } = "";
-    /// <example>false</example>
     public bool IsActive { get; set; } = false;
 }
